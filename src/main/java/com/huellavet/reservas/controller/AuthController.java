62f65e19725af9ad2d93294a21afecddad920e4b
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-09-08 15:53:39 -0500

    endpoints

--- src/main/java/com/huellavet/reservas/controller/AuthController.java
+++ src/main/java/com/huellavet/reservas/controller/AuthController.java
@@ -1,4 +1,23 @@
 package com.huellavet.reservas.controller;
 
+import com.huellavet.reservas.dto.LoginRequest;
+import com.huellavet.reservas.dto.UsuarioResponse;
+import com.huellavet.reservas.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
 public class AuthController {
+    private final AuthService authService;
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+    @PostMapping("/login")
+    public ResponseEntity<UsuarioResponse> iniciarSesion(@Valid @RequestBody LoginRequest request){
+        UsuarioResponse usuario = authService.iniciarSesion(request);
+        return ResponseEntity.ok(usuario);
+    }
 }
